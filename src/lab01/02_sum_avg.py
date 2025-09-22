1e64cf0528a8e6be18e92350966a4c2370e3c98f
Daria Skobeleva <dariella@Darias-MacBook-Air.local>
2025-09-22 11:11:26 +0300

    Done 1-5 tasks

--- src/lab01/02_sum_avg.py
+++ src/lab01/02_sum_avg.py
@@ -1,5 +1,5 @@
 a=float(input('1 число').replace(',','.'))
 b=float(input('2 число').replace(',','.'))
-sum=(a+b)//0.01/100
+sum=a+b
 avg=sum/2
-print(f'sum={sum},avg={avg:.2f}')
+print(f'sum={sum:.2f},avg={avg:.2f}')
--- src/lab01/03_discount_vat.py
+++ src/lab01/03_discount_vat.py
@@ -1,9 +1,9 @@
 price=float(input('цена'))
 discount=float(input('скидка'))
-vat=float(input(''))
-base = price * (1 - discount/100)//0.01/100
-vat_amount = (base * (vat/100))//0.01/100
-total = (base + vat_amount)//0.01/100
-print(f'База после скидки:{base}')
-print(f'НДС:{vat_amount}')
-print(f'И того с оплате:{total}')
+vat=float(input('налог'))
+base = price * (1 - discount/100)
+vat_amount = (base * (vat/100))
+total = (base + vat_amount)
+print(f'База после скидки:{base:.2f}')
+print(f'НДС:{vat_amount:.2f}')
+print(f'И того с оплате:{total:.2f}')
