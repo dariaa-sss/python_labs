71546bf70a4905be062aba57ef3e7c5078a3b84a
dariaa-sss <dariella@Darias-MacBook-Air.local>
2025-10-13 18:16:54 +0300

    01

---
+++ src/lib/text.py
@@ -0,0 +1,28 @@
+import re
+
+pattern = r'\w+(?:-\w+)*'
+def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
+    if casefold: text = text.lower()
+    if yo2e: text = text.replace("ё","е")
+    #text = re.findall(pattern, text)
+    result = re.sub(r'\s+', ' ', text).strip()
+
+    return result
+
+print(normalize("ПрИвЕт\nМИр\t"))
+
+def tokenize(text: str) -> list[str]:
+    return re.findall(pattern, text)
+
+print(tokenize("emoji 😀 не слово"))
+
+def count_freq(tokens: list[str]) -> dict[str, int]:
+    counts: dict[str, int] = {}
+    for token in tokens:
+        counts[token] = counts.get(token, 0) + 1
+    return counts
+print(count_freq(["a","b","b","b","a","c","b","a"]))
+
+def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
+    return sorted(freq.items(), key=lambda x: x[1], reverse=True)[:n]
+print(top_n(count_freq(["a","b","a","c","b","a"]),n=2))
